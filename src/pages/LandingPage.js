fb0301d81ef7698b7a9abf296f0e8d269c87f903
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-20 03:33:50 -0500

    Change: LandingPage to include Backgroundautoplay component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,39 @@
 import styled from "@emotion/styled";
-import BG1 from "../assets/img/bg-airplane.jpg";
+import BackgroundAutoplay from "../components/BackgroundAutoplay";
+import { PrimaryButton } from "../components/Buttons";
+import { colors, typography } from "../styles";
 
-const MainSection = styled.img`
+const MainContainer = styled.div`
+  position: relative;
+  text-align: center;
   width: 100%;
+`;
+const MainSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 15rem;
+  left: 10%;
+  margin: 3rem;
+  align-items:flex-start;
+  transform: translate(-50%, -50%);
+  text-transform: uppercase;
+  ${typography.headline[2]};
+  color: ${colors.secondary[200]};
 `;
 
 export default function LandingPage() {
-  return <MainSection src={BG1}></MainSection>;
+  return (
+    <MainContainer>
+      <BackgroundAutoplay />
+      <MainSection>
+        <span>Live</span>
+        <span>Your</span>
+        <span>Dream</span>
+        <PrimaryButton>
+          Plan your next trip
+        </PrimaryButton>
+      </MainSection>
+    </MainContainer>
+  );
 }
