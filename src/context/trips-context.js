3170d47a5d00abcd8118466323904565acc47458
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-20 10:49:38 -0500

    Add: trips context

---
+++ src/context/trips-context.js
@@ -0,0 +1,33 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { getPosts } from "../services/posts-service";
+
+const TripsContext = createContext();
+
+function TripsProvider({ children }) {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts()
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch(console.log);
+  }, []);
+
+  return (
+    <TripsContext.Provider
+      value={{
+        posts,
+      }}
+    >
+      {children}
+    </TripsContext.Provider>
+  );
+}
+
+function useTrips() {
+  return useContext(TripsContext);
+}
+
+export { TripsProvider, useTrips };
