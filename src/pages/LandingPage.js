80fb4ab5cd49a83aaea2e35f4ceb856d5af80005
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-21 16:03:54 -0500

    Change: landing page main btn

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled";
+import { NavLink } from "react-router-dom";
 import BackgroundAutoplay from "../components/BackgroundAutoplay";
 import { PrimaryButton } from "../components/Buttons";
+import { useAuth } from "../context/auth-context";
 import { colors, typography } from "../styles";
 
 const MainContainer = styled.div`
@@ -15,13 +17,18 @@ const MainSection = styled.div`
   top: 15rem;
   left: 10rem;
   margin: 3rem;
-  align-items:flex-start;
+  align-items: flex-start;
   transform: translate(-50%, -50%);
   ${typography.headline[2]};
   color: ${colors.secondary[200]};
 `;
 
-export default function LandingPage() {
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+`;
+
+export default function LandingPage({ onLoginClick }) {
+  const { user } = useAuth();
   return (
     <MainContainer>
       <BackgroundAutoplay />
@@ -29,9 +36,13 @@ export default function LandingPage() {
         <span>Share</span>
         <span>Read</span>
         <span>Connect</span>
-        <PrimaryButton>
-          Tell us about your last trip
-        </PrimaryButton>
+        {user ? (
+          <StyledNavLink to="/my_trips">
+            <PrimaryButton>Tell us about your last trip</PrimaryButton>
+          </StyledNavLink>
+        ) : (
+          <PrimaryButton onClick={onLoginClick}>Login</PrimaryButton>
+        )}
       </MainSection>
     </MainContainer>
   );
